ffb54d2ab606ffe3a7fbb26dd08f196b7497df7a
enesodabasi <121868912+enesodabasi@users.noreply.github.com>
2023-01-03 23:18:20 +0300

    Create app.R

---
+++ app.R
@@ -0,0 +1,16 @@
+library(shiny)
+runExample("01_hello")
+
+View(mtcars)
+
+runExample("01_mpg")
+ggplot(Mazda RX4)
+~mpgData[[input$variable]]
+
+runGitHub(repo, username = getOption("github.user"), ref = "master",
+          subdir = NULL, destdir = NULL, ...)
+
+library(shiny)
+runGitHub( "MyFirstShiny", "enesodabasi",ref="main")
+
+hist(faithful$waiting,breaks=c(40,50,60,70,80,90,100))
