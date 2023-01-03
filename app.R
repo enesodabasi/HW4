37ae52103b7c033a6242eba4791895ccdd7d86e7
enesodabasi <121868912+enesodabasi@users.noreply.github.com>
2023-01-03 23:56:56 +0300

    Update app.R

--- app.R
+++ app.R
@@ -1,16 +1,53 @@
 library(shiny)
-runExample("01_hello")
+library(datasets)
+library(ggplot2)
 
-View(mtcars)
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
 
-runExample("01_mpg")
-ggplot(Mazda RX4)
-~mpgData[[input$variable]]
+ui <- fluidPage(
+  
+  titlePanel("Miles Per Gallon"),
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      
+      selectInput("variable", "Variable:",
+                  c("Cylinders" = "cyl",
+                    "Transmission" = "am",
+                    "Gears" = "gear")),
+      
+      checkboxInput("outliers", "Show outliers", TRUE)
+      
+    ),
+    
+    mainPanel(
+      
+      h3(textOutput("caption")),
+      
+      plotOutput("mpgPlot")
+      
+    )
+  )
+)
 
-runGitHub(repo, username = getOption("github.user"), ref = "master",
-          subdir = NULL, destdir = NULL, ...)
+server <- function(input, output) {
+ 
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  output$mpgPlot <- renderPlot({
+    ggplot(mpgData,aes(x=mpg)) + 
+    geom_histogram(binwidth = 5,colour="black",fill="white") + 
+      facet_wrap(input$variable, ncol = 1)
+    
+  })
+  
+}
 
-library(shiny)
-runGitHub( "MyFirstShiny", "enesodabasi",ref="main")
-
-hist(faithful$waiting,breaks=c(40,50,60,70,80,90,100))
